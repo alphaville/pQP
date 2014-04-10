e077d6c21b7268f088062d312f95fb85ef3c9337
Pantelis Sopasakis <chvng@mail.ntua.gr>
2014-04-10 17:49:18 +0200

    First source code

---
+++ src/pQP.cu
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <curand.h>
+#include "cblas.h"
+#include "pQP.cuh"
+
+
+
+int pre(real_t *Q, real_t *h, real_t *V, real_t *W) {
+	real_t *Q_init;
+	ptrdiff_t i;
+	if (Q == NULL) {
+		fprintf(stderr, "Uninitialized matrix (Q) at line %d of %s\n", __LINE__,
+				__FILE__);
+		return EXIT_FAILURE;
+	}
+	if (h == NULL) {
+		fprintf(stderr, "Uninitialized matrix (h) at line %d of %s\n", __LINE__,
+				__FILE__);
+		return EXIT_FAILURE;
+	}
+	if (V == NULL) {
+		fprintf(stderr, "Uninitialized matrix (V) at line %d of %s\n", __LINE__,
+				__FILE__);
+		return EXIT_FAILURE;
+	}
+	if (W == NULL) {
+		fprintf(stderr, "Uninitialized matrix (W) at line %d of %s\n", __LINE__,
+				__FILE__);
+		return EXIT_FAILURE;
+	}
+	Q_init = (real_t *) malloc(N * N * sizeof(*Q_init));
+	/* Initialize Q with random data */
+	srand(time(NULL)); // random seed (time-based)
+	for (i = 0; i < N * N; i++) {
+		Q_init[i] = (real_t) ((2 * rand() - RAND_MAX) % 1000 + 1) / 1000.0;
+	}
+	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0, Q_init,
+			N, Q_init, N, 0.0, Q, N);
+	for (i = 0; i < N; i++) {
+		Q[i * (N + 1)] += 2.0 * N;
+		h[i] = (real_t) ((2 * rand() - RAND_MAX) % 1000 + 1) / 1000.0;
+		V[i * (2 * N + 1)] = 1;
+		V[i * (2 * N + 1) + N] = -1;
+		W[i] = 10.0;
+		W[i + N] = -9.0;
+	}
+	free(Q_init);
+	return EXIT_SUCCESS;
+}
+
+
+void compute_matrices(real_t *Q, real_t *h, real_t *V, real_t *W){
+
+}
+
+static void print_matrix(creal_t *A, const unsigned int nRows,
+		const unsigned int nCols) {
+	ptrdiff_t i, j;
+	for (i = 0; i < nRows; i++) {
+		if (i == 0) {
+			printf("[\n");
+		}
+		for (j = 0; j < nCols; j++) {
+			printf("%g ", A[j * nRows + i]);
+		}
+		if (i < nRows - 1) {
+			printf(";\n");
+		} else {
+			printf("]\n");
+		}
+	}
+}
+int main(void) {
+
+	real_t *Q = NULL, *h = NULL, *V = NULL, *W = NULL;
+	ptrdiff_t i, j;
+
+	Q = (real_t *) malloc(N * N * sizeof(*Q));
+	h = (real_t *) malloc(N * sizeof(*h));
+	V = (real_t *) malloc(2 * N * N * sizeof(*V));
+	W = (real_t *) malloc(2 * N * sizeof(*W));
+	pre(Q, h, V, W);
+
+	for (i = 0; i < N; i++)
+		printf("h[%td]=%3.2f\n", i, h[i]);
+
+	print_matrix(V, 2 * N, N);
+	print_matrix(W, 2 * N, 1);
+
+	return 0;
+}
---
+++ src/pQP.cuh
@@ -0,0 +1,45 @@
+/*
+ * pQP.cuh
+ *
+ *      Created on: Feb 27, 2014
+ *      Author: Pantelis Sopasakis
+ */
+
+#ifndef PQP_CUH_
+#define PQP_CUH_
+
+#include "cblas.h"
+
+#define CUDA_CALL(value) do {		           								\
+	cudaError_t _m_cudaStat = value;										\
+	if (_m_cudaStat != cudaSuccess) {										\
+		fprintf(stderr, "Error %s at line %d in file %s\n",					\
+				cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);		\
+		exit(1);        													\
+	} } while(0)
+
+#define CUBLAS_CALL(value) do {                                                           \
+	cublasStatus_t _m_status = value;                                                             \
+	if (_m_status != CUBLAS_STATUS_SUCCESS){                                                      \
+		fprintf(stderr, "Error %d at line %d in file %s\n", (int)_m_status, __LINE__, __FILE__);  \
+	exit(14);                                                                                     \
+	}                                                                                             \
+	} while(0)
+
+#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) {  \
+    printf("Error at %s:%d\n",__FILE__,__LINE__);             \
+    exit(115);}} while(0)
+
+#define N 10
+
+typedef double real_t;
+typedef const real_t creal_t;
+
+int pre(real_t *Q, real_t *h, real_t *V, real_t *W);
+
+static void print_matrix(creal_t *A, const unsigned int nRows,
+		const unsigned int nCols);
+
+
+
+#endif /* PQP_CUH_ */
